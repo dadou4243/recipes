b78636a0b85a7c938643ca54d2dada16bca46583
David Lasry <davidlasry@hotmail.com>
2018-08-04 22:32:49 +0300

    Initial Heroku commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+const api = require('./server/routes/recipes');
+
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+//app.use('/api', api);
+
+app.get('*', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+
+})
+
+const port = process.env.PORT || 4600;
+
+app.listen(port, (req, res) => {
+    console.log(`RUNNING on port ${port}`);
+});
